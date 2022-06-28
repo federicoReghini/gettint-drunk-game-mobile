b628096e0a543dceb781b6b242ad342566b83ca4
Federico Reghini <federicoreghini@gmail.com>
2022-06-28 17:31:10 +0200

    home

--- screens/Home.js
+++ screens/Home.js
@@ -1,25 +1,12 @@
 import React from 'react';
 
 // library components
-import { clearStorage, HomeNf, getStorage, setStorage } from 'gettint-drunk';
+import { clearStorage, HomeNf } from 'gettint-drunk';
 import { View } from 'react-native';
 
 
 const Home = ({ navigation }) => {
   // clearStorage()
-
-  (async () => {
-   
-    const res = await getStorage('token');
-    const res1 = await getStorage('refreshToken');
-    const res2 = await getStorage('user');
-    console.log('res',res);
-    console.log('res1',res1);
-    console.log('res2',res2);
-  })()
-  const handleQuickMatch = () => {
-
-  }
 
   const handleNavigation = (path) => () => {
     navigation.navigate(path);
@@ -28,7 +15,7 @@ const Home = ({ navigation }) => {
   return (
     <View style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       <HomeNf
-        onQuickMatch={handleQuickMatch}
+        onQuickMatch={handleNavigation('JoinLobby')}
         onCreateLobby={handleNavigation('CreatesLobby')}
         onLeaderBoard={handleNavigation('Leaderboard')}
       />
